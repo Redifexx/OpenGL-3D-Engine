e903effe75880ca076bcde865a88d5a798ecb940
Giovanni Perez Colon <gperezcolon@ufl.edu>
2023-01-19 20:50:31 -0500

    INDICES

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -14,7 +15,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"   FragColor = vec4(0.9f, 0.8f, 0.02f, 1.0f);\n"
 "}\n\0";
 
 
@@ -32,7 +33,18 @@ int main()
 	{
 		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
 		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f
+	};
+
+
+	GLuint indices[] =
+	{
+		0, 3, 5,
+		3, 2, 4,
+		5, 4, 1
 	};
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Yo", NULL, NULL);
@@ -67,15 +79,19 @@ int main()
 	glDeleteShader(fragmentShader);
 
 
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -83,11 +99,13 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
-	glClearColor(0.0f, 0.13f, 0.17f, 1.0f); //set window color
-	glClear(GL_COLOR_BUFFER_BIT); //set color buffer
-	glfwSwapBuffers(window); //swap buffers
+
+	//glClearColor(0.0f, 0.13f, 0.17f, 1.0f); //set window color
+	//glClear(GL_COLOR_BUFFER_BIT); //set color buffer
+	//glfwSwapBuffers(window); //swap buffers
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -95,7 +113,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT); //set color buffer
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents(); //update window
@@ -103,6 +121,7 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 
